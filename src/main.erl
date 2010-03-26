a9cd94142e7a22c53d5f86402963c8a74509e228
wvd <william@williaminc.nl>
2010-03-26 14:10:10 +0100

    src directory

---
+++ src/main.erl
@@ -0,0 +1,24 @@
+%% Author: Doorn
+%% Created: 26 mrt 2010
+%% Description: TODO: Add description to main
+-module(main).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
